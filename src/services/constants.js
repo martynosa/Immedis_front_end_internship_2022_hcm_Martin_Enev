4d611772662fc1c6e7f9f0b7baa0e0474ed75486
martiN <martynosa@hotmail.com>
2022-07-05 09:40:53 +0300

    heroku api

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,3 +1,7 @@
-export const AUTH_URL = 'http://localhost:5000/auth';
-export const EMPLS_URL = 'http://localhost:5000/employees';
-export const PHOTO_URL = 'http://localhost:5000/users';
+// export const AUTH_URL = 'http://localhost:5000/auth';
+// export const EMPLS_URL = 'http://localhost:5000/employees';
+// export const PHOTO_URL = 'http://localhost:5000/users';
+
+export const AUTH_URL = 'https://heroku-hr-server.herokuapp.com/auth';
+export const EMPLS_URL = 'https://heroku-hr-server.herokuapp.com/employees';
+export const PHOTO_URL = 'https://heroku-hr-server.herokuapp.com/users';
